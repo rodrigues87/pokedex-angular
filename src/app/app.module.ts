e188e5fd02d92fee53d11525f46e382d931f54f9
sala19a <pedroh.mix@gmail.com>
2022-05-19 20:03:43 -0300

    Criando tollbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        MatToolbarModule,
+        NgbModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
